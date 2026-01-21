49aa01caeea63ac308c69ed6ec58eb7059fdcb32
Ariel Guevara <adguevara7@espe.edu.ec>
2026-01-21 11:29:45 -0500

    initializate project

---
+++ src/main/java/ec/edu/espe/e_comerce_core/EComerceCoreApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.e_comerce_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComerceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComerceCoreApplication.class, args);
+	}
+
+}
